2ba6be9c6024ae23629cd492323fa764f36574db
rpj2 <rpj2>
2019-03-23 03:26:09 -0600

    added simplelog.properties

--- build/root/install.sh
+++ build/root/install.sh
@@ -48,6 +48,9 @@ ln -s /config/streambaby.ini /streambaby-0.54/streambaby.ini
 touch /config/streambaby.log
 ln -s /config/streambaby.log /streambaby-0.54/streambaby.log
 
+mv /streambaby-0.54/simplelog.properties /config/
+ln -s /config/simplelog.properties /streambaby-0.54/simplelog.properties
+
 mv /streambaby-0.54/cache /
 ln -s /cache /streambaby-0.54/cache
 
